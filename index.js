24896abf34fd9295a2f4ad4fc926e5ccd8370a22
vinay singh <72579940+vinaysingh17@users.noreply.github.com>
2023-05-05 19:36:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,34 @@ const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.port || 5000;
 require("./CONN_CONST/Connection");
+// -----------------
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.json({ limit: "50mb" }));
+
+var cors = require("cors");
+app.use(cors({ origin: true, credentials: true }));
+app.use(function (req, res, next) {
+  console.log(req._parsedUrl.path, "----<<<<<<<<<<<Current ");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
+// -------------------------------------
+// Routes
+// const AuthRoutes = require("./ROUTES/Auth.Routes");
+app.use("/static", express.static("uploads"));
+// Pages 
+// app.use("/auth", AuthRoutes);
 
 app.listen(PORT, () => console.log(PORT));
